3199e1b601302cbdf67a965edc86add08a472a66
sukrugultop <sukrucan@sociality.io>
2019-11-19 19:40:51 +0300

    add say action

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,8 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'hello') {
+        return $this->render('say', ['message' => $message]);
+    }
 }
